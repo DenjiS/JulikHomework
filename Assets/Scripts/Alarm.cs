d5a4da68c6415ffff5120bc37283b4437efbe3f0
Daniel <Sychkov.Daniil@yandex.ru>
2023-07-04 00:36:06 +0300

    Update Alarm.cs
    Removed duplicated code and magical numbers

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -7,6 +7,9 @@ public class Alarm : MonoBehaviour
 {
     [SerializeField] private float _volumeGrowthIntensity;
 
+    private readonly float _maxVolume = 1;
+    private readonly float _minVolume = 0;
+
     private AlarmArea[] _areas;
     private AudioSource _audioSource;
 
@@ -27,16 +30,12 @@ public class Alarm : MonoBehaviour
 
     private void StartAlarm()
     {
-        if (_volumeMoveCoroutine != null)
-        {
-            StopCoroutine(_volumeMoveCoroutine);
-        }
-        else
+        if (TryStopPreviousCoroutine() == false)
         {
             _audioSource.Play();
         }
 
-        _volumeMoveCoroutine = StartCoroutine(MoveVolume(1));
+        _volumeMoveCoroutine = StartCoroutine(MoveVolume(_maxVolume));
     }
 
     private void StopAlarm(Transform player)
@@ -44,12 +43,20 @@ public class Alarm : MonoBehaviour
         if (_areas.Any(area => area.Bounds.Contains(player.position)))
                 return;
 
+        TryStopPreviousCoroutine();
+
+        _volumeMoveCoroutine = StartCoroutine(MoveVolume(_minVolume));
+    }
+
+    private bool TryStopPreviousCoroutine()
+    {
         if (_volumeMoveCoroutine != null)
         {
             StopCoroutine(_volumeMoveCoroutine);
+            return true;
         }
 
-        _volumeMoveCoroutine = StartCoroutine(MoveVolume(0));
+        return false;
     }
 
     private IEnumerator MoveVolume(float toValue)
